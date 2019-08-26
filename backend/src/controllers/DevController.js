6755d9979d01e557f3ffd87a26dba59ee7dd2482
Thiago Anjos <thiagowaltrick@gmail.com>
2019-08-26 19:10:06 -0300

    não permitir cadastro duplicado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -12,6 +12,19 @@ module.exports = {
         const { username } = req.body;
         const testeUsername = req.body.username; //acho que tbm da pra usar assim
 
+
+        //verificar se o usuário existe na base antes de salvar.
+        //                     procura um user que seja igual a username
+        const userExists = await Dev.findOne({ user : username});
+        if(userExists)
+        {
+            console.log("Usuário ja existia na DB\nNão foi necessário salvar...");
+            return res.json(userExists);
+        }
+        // não precisa fazer else nesse caso.
+        // teoricamente o js não executa mais após o return
+        // nesse caso se esse if for verdadeiro ele não passara daqui.
+
         // o await faz com que o node espere até que essa função retorne e exista.
         // ao utilizar o await, utilize tbm o async nesse objeto
         //const responseOfAxios = await axios.get(`https://api.github.com/users/${testeUsername}`); 
